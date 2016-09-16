174735eb1dbb869b7f4a5912f8541db12ffd6b58
Kyle Luce <nomadmtb@gmail.com>
2016-09-15 22:19:02 -0500

    Adding config options etc...
    Adding config options etc...

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('config.DevConfig')
 db = SQLAlchemy(app)
 
 @app.errorhandler(404)
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 # Run the test server with this file here...
 from app import app
+#app.run(host='0.0.0.0', port=8080, debug=False)
 
-app.run(host='0.0.0.0', port=8080, debug=False)
+app.run(
+    host=app.config['SERVER_NAME'].split(':')[0],
+    port=int(app.config['SERVER_NAME'].split(':')[1]),
+    debug=app.config['DEBUG']
+)
