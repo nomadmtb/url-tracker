f22e1c196e6baf129112b1da875decdda8e12073
Kyle Luce <nomadmtb@gmail.com>
2016-10-02 22:28:18 -0500

    Description Meta + config changes
    Description Meta + config changes

--- config.py
+++ config.py
@@ -1,17 +1,15 @@
 # Config variables for the application...
-import os
+import os, string, random
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
 DATABASE_CONNECT_OPTIONS = {}
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 THREADS_PER_PAGE = 2
 CSRF_ENABLED = True
-CSRF_SESSION_KEY = 'AsdaSDeqiiw78!*hajhsd*1*71jsjah@4Asd'
-SECRET_KEY = 'AaskdASdmA*1273aSDHasy86123@12ja'
+CSRF_SESSION_KEY = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(50)])
+SECRET_KEY = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(50)])
 
-SERVERNAME = '127.0.0.1:8080' # dev
-#SERVERNAME = 'http://utldr.co' # pro
-
-DEBUG = True # dev
-#DEBUG = False #pro
+SERVERNAME = '127.0.0.1:8080' if DEBUG else 'http://utldr.co'
