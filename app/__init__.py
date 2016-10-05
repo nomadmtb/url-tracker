872bb0d9acea127f28eca87ec4f58604624ff0ab
Kyle Luce <nomadmtb@gmail.com>
2016-10-04 20:39:53 -0500

    Adding server error page…

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,10 @@ db = SQLAlchemy(app)
 def not_found(error):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html'), 500
+
 from app.url_tracker.controllers import url_tracker as tracker_module
 app.register_blueprint(tracker_module)
 
